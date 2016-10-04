ed40a8da9ae97d4332d5a8e737e16e86807c9ec9
Allison Hoke <allisonhoke@gmail.com>
2016-10-04 13:53:42 -0700

    created routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  resources :market, only: [:index, :edit, :create, :show] do
+    resources :vendor, only: [:create, :edit, :delete, :index, :show]
+  end
+
+  resources :vendor do
+    resources :products, only: [:create, :edit, :delete,:index, :show]
+    resources :sales, only: [:create, :show, :index]
+  end
+
+  resources :market, only: [:index, :show]
+  resources :vendors, only: [:index]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
