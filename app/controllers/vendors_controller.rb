a06ee871242a5b686097581d0814b81417f24677
Natasha Ewing <natasharai@me.com>
2016-10-05 15:46:21 -0700

    changed vendors controller name

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,28 +1,26 @@
 class VendorsController < ApplicationController
   def index
-    @Vendors = Vendors.all
+    @vendors = Vendors.all
   end
 
   def show
     @Vendor = Vendor.find(params[:id])
-    @s = @Vendor.all_s
+    @vendor.save
   end
 
   def new
-    @Vendor = Vendor.new
+    @vendor = Vendor.new
   end
 
   def create
-    @Vendor = Vendor.new(Vendor_params)
+    @vendor = Vendor.new(vendor_params)
 
-    if @Vendor.save
+    if @vendor.save
+    end
   end
-
   private
 
   def Vendor_params
-    params.require(:Vendor).permit(:name, :employees, :market_id)
+    params.require(:vendor).permit(:id, :name, :number_employees, :market_id)
   end
-end
-
 end
