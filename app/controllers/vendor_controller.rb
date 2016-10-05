6073e2ce835840a45295edcde9a3d8388169ec7e
Natasha Ewing <natasharai@me.com>
2016-10-05 14:52:53 -0700

    vendor controller methods, added some of the html.erb pages, also, note that I changed the welcome links.

---
+++ app/controllers/vendor_controller.rb
@@ -0,0 +1,28 @@
+class Controller < ApplicationController
+  def index
+    @Vendors = .all
+  end
+
+  def show
+    @Vendor = Vendor.find(params[:id])
+    @s = @Vendor.all_s
+  end
+
+  def new
+    @Vendor = Vendor.new
+  end
+
+  def create
+    @Vendor = Vendor.new(Vendor_params)
+
+    if @Vendor.save
+  end
+
+  private
+
+  def Vendor_params
+    params.require(:Vendor).permit(:name, :employees, :market_id)
+  end
+end
+
+end
