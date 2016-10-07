9ba30aaff70dc899f972bd4c40c2c7a7d80e0eed
Allison Hoke <allisonhoke@gmail.com>
2016-10-07 14:31:26 -0700

    figured out how to add product id to new sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -10,4 +10,23 @@ class SalesController < ApplicationController
 
     @this_month_sales_total = @vendor.current_month_sales_total
   end
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new
+    @sale.amount = params[:sale][:amount]
+    @sale.purchase_time = Time.now
+    @sale.vendor_id = params[:vendor_id]
+    @sale.product_id = params[:sale][:product_id]
+
+    if @sale.save
+      redirect_to vendor_sales_path
+    else
+      render :new
+    end
+  end
+
 end
