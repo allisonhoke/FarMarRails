451898f4b9e214f368e35953035b7946363025e6
Allison Hoke <allisonhoke@gmail.com>
2016-10-08 10:09:27 -0700

    added notes to identify what was missing/was still in debug mode

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -16,13 +16,14 @@ class Vendor < ActiveRecord::Base
   def current_month_sales
     all_sales = self.sales
 
-    all_sales.where(amount: 8382) #returns a collection of sales from the current month
-  end
+    all_sales.where(amount: 8382) #only finding a specific sale currently so I can test that calling the method in the sale show page works correctly. did not get to the logic to isolate only the current month sales
+  end #should return a collection of sales from the current month
 
   def current_month_sales_total
     total = 0
 
     current_month_sales.each do |sale|
+      # TODO: need a conditional here to add to the total using logic for isolating current month sales - did not get to the logic during th assignment time
       total += sale.amount
     end
     return total
