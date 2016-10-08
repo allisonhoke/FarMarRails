f8121f2704a05d38214b18f6992b2b40f74d9d1f
Allison Hoke <allisonhoke@gmail.com>
2016-10-08 10:05:54 -0700

    removed unnecessary methods that belong in the model file

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,16 +1,2 @@
 module SalesHelper
-  def current_month_sales
-    all_sales = self.sales
-
-    all_sales.where(amount: 8382) #returns a collection of sales from the current month
-  end
-
-  def current_month_sales_total
-    total = 0
-
-    current_month_sales.each do |sale|
-      total += sale.amount
-    end
-    return total
-  end
 end
